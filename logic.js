ec9493b13d25546bdec898a3cf1561f63059017a
jelly octane <jamie.de.lange@mac.com>
2020-10-25 22:15:15 -0500

    debug scorelist

--- logic.js
+++ logic.js
@@ -1,1 +1,26 @@
 var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+
+var listScoresEl = document.createElement("ol");
+
+for (var i = 0; i < highScores.length; i++) {
+    var scoreListEl = document.createElement("li");
+    scoreListEl.appendChild(listScoresEl);
+}
+
+scoreListEl.appendChild(scorelist);
+
+
+
+
+
+
+    
+    
+    
+    
+    /* var listScores = document.createElement("li");
+    listScores.className = "user-initials-class";
+    listScores.setAttribute("id", "user-initials" + user-initials);
+    listScores.setAttribute("value", enterInitial);
+    listScores.textContent = enterInitial + " " + highScores;
+    scorelist.appendChild(listScores); */
